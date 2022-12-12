1724cb1787b9c33ff6964ccd79cc4c52914d82cf
Nuro <82734446+NuroC@users.noreply.github.com>
2022-12-12 07:19:49 -0500

    :art: Improvement and compatibility fix
    fixed weird thing in tsconfig, and changed the websocket hook from overriding the websocket class to websocket.prototype.send, so people wiwth lag issues can use it

--- src/ws/hookWS.ts
+++ src/ws/hookWS.ts
@@ -2,21 +2,27 @@ import decode from "../lib/decode.js";
 import { handlePacket } from "./handlePacket";
 import MooMoo from "../index";
 
+let _onmessage: boolean = false;
+
 export default function hookWS() {
-    WebSocket = new Proxy(WebSocket, {
-        construct(target: any, args) {
-            MooMoo.ws = Reflect.construct(target, args);
-            MooMoo.ws.addEventListener("message", (e: any) => {
-                let data = e.data;
-                try {
-                    let decoded = decode(data);
-                    let [packet, [...packetData]] = decoded;
-                    handlePacket(packet, packetData);
-                } catch (e) {
-                    throw new Error(e);
-                }
-            });
-            return MooMoo.ws;
+
+    WebSocket.prototype.send = new Proxy(WebSocket.prototype.send, {
+        apply(target, thisArg, args) {
+            MooMoo.ws = thisArg;
+            if (!_onmessage) {
+                _onmessage = true;
+                MooMoo.ws.addEventListener("message", (e: any) => {
+                    let data = e.data;
+                    try {
+                        let decoded = decode(data);
+                        let [packet, [...packetData]] = decoded;
+                        handlePacket(packet, packetData);
+                    } catch (e) {
+                        throw new Error(e);
+                    }
+                })
+            }
+            return Reflect.apply(target, thisArg, args);
         }
     });
 }
