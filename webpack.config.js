6c7b699b529abcd88b2bef30e497cb2866c9770a
Nuro <82734446+NuroC@users.noreply.github.com>
2022-12-12 08:06:13 -0500

    :package: added clean mode

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     optimization: {
         minimize: false,
