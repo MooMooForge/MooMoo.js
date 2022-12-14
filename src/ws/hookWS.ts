3d1c74f32cb0377f2d7ee9d4a138987b7b1266d5
Nuro <82734446+NuroC@users.noreply.github.com>
2022-12-14 10:19:11 -0500

    :ambulance: sendPacket was not working.

--- src/ws/hookWS.ts
+++ src/ws/hookWS.ts
@@ -10,6 +10,13 @@ export default function hookWS() {
     WebSocket.prototype.send = new Proxy(WebSocket.prototype.send, {
         apply(target, thisArg, args) {
             MooMoo.ws = thisArg;
+            MooMoo.sendPacket = function (type: string) {
+                let data = Array.prototype.slice.call(arguments, 1);
+                let binary = encode([type, data]);
+
+                MooMoo.ws.send(binary);
+            }
+            if (MooMoo.ws.readyState !== 1) return true;
             if (!_onmessage) {
                 _onmessage = true;
                 MooMoo.ws.addEventListener("message", (e: any) => {
@@ -17,13 +24,6 @@ export default function hookWS() {
                     let decoded = decode(data);
                     let [packet, [...packetData]] = decoded;
                     handlePacket(packet, packetData);
-
-                    MooMoo.sendPacket = function (type: string) {
-                        let data = Array.prototype.slice.call(arguments, 1);
-                        let binary = encode([type, data]);
-
-                        MooMoo.ws.send(binary);
-                    }
                 })
 
 
