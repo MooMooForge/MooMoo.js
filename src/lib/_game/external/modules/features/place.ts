cb6a7ff4ccf4e8840d7331ea45f81681da1f6203
Nuro <america.alex.arn@icloud.com>
2022-12-17 12:55:21 -0500

    :recycle: fixed place

--- src/lib/_game/external/modules/features/place.ts
+++ src/lib/_game/external/modules/features/place.ts
@@ -1,11 +1,11 @@
 import { MooMoo } from "../../../../../../app";
 
 function place (id: Number, angle: Number) {
-    let inventory = MooMoo.myPlayer.inventory;
+    let weapon = MooMoo.myPlayer.weaponIndex
     MooMoo.sendPacket("5", id, angle)
     MooMoo.sendPacket("c", 1, angle)
     MooMoo.sendPacket("c", 0, angle)
-    MooMoo.sendPacket("5", inventory.primary, true)
+    MooMoo.sendPacket("5", weapon, true)
 }
 
 export default place
