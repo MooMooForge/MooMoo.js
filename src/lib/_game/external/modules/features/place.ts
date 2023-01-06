e29129e0e97a684db93f7edd43c251ebb6887974
Nuro <82734446+NuroC@users.noreply.github.com>
2023-01-06 12:00:26 -0500

    Fix place :construction:

--- src/lib/_game/external/modules/features/place.ts
+++ src/lib/_game/external/modules/features/place.ts
@@ -2,7 +2,7 @@ import { MooMoo } from "../../../../../../app";
 
 function place (id: Number, angle: Number) {
     let weapon = MooMoo.myPlayer.weaponIndex
-    MooMoo.sendPacket("5", id, angle)
+    MooMoo.sendPacket("5", id, false)
     MooMoo.sendPacket("c", 1, angle)
     MooMoo.sendPacket("c", 0, angle)
     MooMoo.sendPacket("5", weapon, true)
