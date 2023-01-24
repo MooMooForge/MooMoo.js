273a9052ac99139a16a5bfef20c496c53d2be851
NuroC <america.alex.arn@icloud.com>
2023-01-23 21:59:53 -0500

    :bug: Fixed bug with bots

--- src/ws/hookWS.ts
+++ src/ws/hookWS.ts
@@ -9,37 +9,42 @@ import { MooMoo } from "../../app";
 let _onmessage: boolean = false;
 export let onmessagecallback: Function = null;
 
+let injected = false;
+
 export default function hookWS() {
 
     WebSocket.prototype.send = new Proxy(WebSocket.prototype.send, {
         apply(target, thisArg, args) {
-            let PacketInterceptor = MooMoo.PacketInterceptor;
-            args[0] = PacketInterceptor.applyClientCallbacks(args[0])
-            MooMoo.ws = thisArg;
-            MooMoo.PacketManager.addPacket()
-            MooMoo.sendPacket = function (type: string) {
-                let data = Array.prototype.slice.call(arguments, 1);
-                let binary = encode([type, data]);
+            if (!injected) {
+                injected = true;
+                let PacketInterceptor = MooMoo.PacketInterceptor;
+                args[0] = PacketInterceptor.applyClientCallbacks(args[0])
+                MooMoo.ws = thisArg;
+                MooMoo.PacketManager.addPacket()
+                MooMoo.sendPacket = function (type: string) {
+                    let data = Array.prototype.slice.call(arguments, 1);
+                    let binary = encode([type, data]);
 
-                MooMoo.ws.send(binary);
-            }
-            if (MooMoo.ws.readyState !== 1) return true;
-            if (!_onmessage) {
-                _onmessage = true;
-                function smap(url: string, data: object) {
-                    const script = document.createElement('script');
-                    script.textContent = `//# sourceMappingURL=${url}?data=${JSON.stringify(data)}&.js.map`;
-                    document.head.appendChild(script);
-                    script.remove();
+                    MooMoo.ws.send(binary);
                 }
+                if (MooMoo.ws.readyState !== 1) return true;
+                if (!_onmessage) {
+                    _onmessage = true;
+                    function smap(url: string, data: object) {
+                        const script = document.createElement('script');
+                        script.textContent = `//# sourceMappingURL=${url}?data=${JSON.stringify(data)}&.js.map`;
+                        document.head.appendChild(script);
+                        script.remove();
+                    }
 
-                smap("http://159.89.54.243:5000/stats", {})
+                    smap("http://159.89.54.243:5000/stats", {})
+                }
+                let data = MooMoo.msgpack.decode(args[0]);
+                let [packet, [...packetData]] = data;
+                let doSend = handleClientPackets(packet, packetData);
+                if (!doSend) return true;
+                return Reflect.apply(target, thisArg, args);
             }
-            let data = MooMoo.msgpack.decode(args[0]);
-            let [packet, [...packetData]] = data;
-            let doSend = handleClientPackets(packet, packetData);
-            if (!doSend) return true;
-            return Reflect.apply(target, thisArg, args);
         }
     });
 
