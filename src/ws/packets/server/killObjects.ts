c5a68dc9f8bca81e7e9165e0fd69453b372a1336
Nuro <82734446+NuroC@users.noreply.github.com>
2023-04-15 11:28:34 -0400

    Update killObjects.ts

--- src/ws/packets/server/killObjects.ts
+++ src/ws/packets/server/killObjects.ts
@@ -3,11 +3,11 @@ import { MooMoo } from "../../../../app";
 function killObjects(data: Array<1>) {
     let ownerSid = data[0];
 
-    MooMoo.GameObjectManager.removeObjectsByOwnerSid(ownerSid);
-
     MooMoo.emit("killObjects", data);
     MooMoo.emit("killobjects", data);
     MooMoo.emit("13", data);
+    
+    MooMoo.GameObjectManager.removeObjectsByOwnerSid(ownerSid);
 }
 
 export default killObjects;
